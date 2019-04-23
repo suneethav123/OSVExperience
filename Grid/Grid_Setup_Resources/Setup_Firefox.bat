674f922d4779fc8b0c232f8e9fd2353a88231eba
suneethav123 <40072748+suneethav123@users.noreply.github.com>
2019-04-23 18:12:55 -0500

    Update Setup_Firefox.bat
    checkin

--- Grid/Grid_Setup_Resources/Setup_Firefox.bat
+++ Grid/Grid_Setup_Resources/Setup_Firefox.bat
@@ -1,1 +1,1 @@
-java -jar selenium-server-standalone-2.53.0.jar -port 7777 -role node -hub http://localhost:4444/grid/register -browser browserName=firefox,maxInstances=4,platform=WINDOWS
+java -jar selenium-server-standalone-2.53.0.jar -port 7777 -role node -hub http://localhost:4444/grid/register -browser browserName=firefox,maxInstances=3,platform=WINDOWS
